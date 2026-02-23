a7c1833b961dcc863e3070b489cc3639584884e7
Mr-Ilyas-Nour <187109104+Ilyas-Nour@users.noreply.github.com>
2026-02-23 02:33:46 +0000

    fix: restore correctly formatted next.config.js

--- next.config.js
+++ next.config.js
@@ -80,38 +80,38 @@ const nextConfig = {
     ],
   },
   async headers() {
-    rn[
-
-      ce: '/:path*',
-        ers: [
-
-          'X-DNS-Prefetch-Control',
-          e: 'on'
-          
-          
-             'Strict-Transport-Security',
-          e: 'max-age=63072000; includeSubDomains; preload'
-          
-          
-             'X-Frame-Options',
-          e: 'SAMEORIGIN'
-          
-          
-             'X-Content-Type-Options',
-          e: 'nosniff'
-          
-          
-             'Referrer-Policy',
-          e: 'origin-when-cross-origin'
-          
-          
-             'Permissions-Policy',
-          e: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
-          
-        
-      
-    
-  
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
